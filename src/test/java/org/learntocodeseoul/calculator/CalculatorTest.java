576509d2ba41dcbf91b251e76a4f54465280cbb8
Ben S <bsadick@gmail.com>
2016-11-26 16:21:06 +0900

    added unit testing

--- src/test/java/org/learntocodeseoul/calculator/CalculatorTest.java
+++ src/test/java/org/learntocodeseoul/calculator/CalculatorTest.java
@@ -2,6 +2,8 @@ package org.learntocodeseoul.calculator;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -10,23 +12,36 @@ import static org.junit.Assert.fail;
  */
 public class CalculatorTest {
     private Calculator calc = new Calculator();
+    private Random rand = new Random();
 
     @Test
     public void testPlus(){
-        assertEquals(2, calc.plus(1,1));
-        assertEquals("1 plus 1 must be 2", 3, calc.plus(1,2));
+        long rand1 = rand.nextLong();
+        long rand2 = rand.nextLong();
+        long expected = rand1 + rand2;
+        assertEquals(rand1 + " plus " + rand2 + "must be " + expected, expected , calc.plus(rand1,rand2));
     }
     @Test
     public void testMinus(){
-
+        long rand1 = rand.nextLong();
+        long rand2 = rand.nextLong();
+        long expected = rand1 - rand2;
+        assertEquals(rand1 + " minus " + rand2 + "must be " + expected, expected , calc.minus(rand1,rand2));
     }
     @Test
     public void testDivide(){
+        long rand1 = rand.nextLong();
+        long rand2 = rand.nextLong();
+        long expected = rand1 / rand2;
+        assertEquals(rand1 + " divided by " + rand2 + "must be " + expected, expected , calc.divide(rand1,rand2));
 
     }
     @Test
     public void testMult(){
-
+        long rand1 = rand.nextLong();
+        long rand2 = rand.nextLong();
+        long expected = rand1 * rand2;
+        assertEquals(rand1 + " times " + rand2 + "must be " + expected, expected , calc.times(rand1,rand2));
     }
 
 }
