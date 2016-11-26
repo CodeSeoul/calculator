06760510cf1be6c26ecf0f2ef0bad56ed8f68b96
Ben S <bsadick@gmail.com>
2016-11-26 16:45:15 +0900

    temp commit

--- src/main/java/org/learntocodeseoul/calculator/Calculator.java
+++ src/main/java/org/learntocodeseoul/calculator/Calculator.java
@@ -2,6 +2,12 @@ package org.learntocodeseoul.calculator;
 
 public class Calculator {
 
+	public long choice(String operator, long first, long second) {
+		if (operator.equals("plus")){
+			return plus(first, second);
+		}
+		return -1;
+	}
 	public long plus(long first, long second) {
 		return first + second;
 	}
