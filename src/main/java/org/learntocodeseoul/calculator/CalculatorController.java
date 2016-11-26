c5997c640a685467081049364c6304eb6b94ad94
Ben S <bsadick@gmail.com>
2016-11-26 15:47:58 +0900

    started making code modularized

--- src/main/java/org/learntocodeseoul/calculator/CalculatorController.java
+++ src/main/java/org/learntocodeseoul/calculator/CalculatorController.java
@@ -1,5 +1,6 @@
 package org.learntocodeseoul.calculator;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +10,31 @@ public class CalculatorController {
 
 	private Calculator calculator = new Calculator();
 
-	@RequestMapping("/plus")
-	public Response plus(@RequestParam String first, @RequestParam String second) {
+	@RequestMapping("/calculate/{operation}")
+	public Response calculate(@PathVariable String operation, @RequestParam String first, @RequestParam String second) {
+		long firstLong;
+		long secondLong;
 		try {
-			long firstLong = Long.parseLong(first);
-			long secondLong = Long.parseLong(second);
-			long sum = calculator.plus(firstLong, secondLong);
-			return new Response("SUCESS", sum);
+			firstLong = Long.parseLong(first);
+			secondLong = Long.parseLong(second);
+		} catch (Exception e) {
+			return new Response("FAILURE", -1);
+		}
+
+		try {
+			long answer = -1;
+			if (operation.equals("plus")) {
+				answer = calculator.plus(firstLong, secondLong);
+			}
+			return new Response("SUCESS", answer);
 		} catch(Exception e) {
 			return new Response("FAILURE", -1);
 		}
+	}
+
+	@RequestMapping("/plus")
+	public Response plus(@RequestParam String first, @RequestParam String second) {
+		return calculate("plus", first, second);
 	}
 
 	@RequestMapping("/minus")
