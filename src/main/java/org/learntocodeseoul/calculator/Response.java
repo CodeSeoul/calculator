dfbd6aebcad20e0827a9f33196836fac29cf8bf2
Ben S <bsadick@gmail.com>
2016-12-01 20:54:27 +0900

    added constructor to the response class

--- src/main/java/org/learntocodeseoul/calculator/Response.java
+++ src/main/java/org/learntocodeseoul/calculator/Response.java
@@ -4,14 +4,21 @@ public class Response {
 
 	private String status;
 	private long result;
+	private String errorMessage;
 
 	public Response() {
 	}
 
-	public Response(String status, long result) {
+	public Response(String status, long result){
 		this.status = status;
 		this.result = result;
 	}
+
+	public Response(String status, long result, String errorMessage) {
+		this(status, result);
+		this.errorMessage = errorMessage;
+	}
+
 
 	public String getStatus() {
 		return status;
@@ -27,5 +34,13 @@ public class Response {
 
 	public void setResult(long result) {
 		this.result = result;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
 	}
 }
